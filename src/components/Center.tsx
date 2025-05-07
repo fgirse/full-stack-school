c5debeb96a77120f66df751c8d4913a3e5478157
Frank Girse <fgirse@mac-mini-von-frank.home>
2025-05-07 23:23:28 +0200

    14er commit

--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Center: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className='max-w-7xl mx-auto p-[0 20px]'>
+    <div className='bg-slate-500 max-w-7xl mx-auto p-[0 20px]'>
         {children}
     </div>
   )
