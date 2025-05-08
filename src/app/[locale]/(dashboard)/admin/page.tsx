2325277cc4027552061e3e06a8797134e2688e0c
Frank Girse <fgirse@mac-mini-von-frank.home>
2025-05-08 17:09:06 +0200

    commit 20er

--- src/app/[locale]/(dashboard)/admin/page.tsx
+++ src/app/[locale]/(dashboard)/admin/page.tsx
@@ -5,11 +5,11 @@ import EventCalendarContainer from "@/components/EventCalendarContainer";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
-const AdminPage = ({
+function AdminPage({
   searchParams,
 }: {
-  searchParams: { [keys: string]: string | undefined };
-}) => {
+  searchParams: { [keys: string]: string | undefined; };
+}) {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
@@ -39,11 +39,11 @@ const AdminPage = ({
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendarContainer searchParams={searchParams}/>
+        <EventCalendarContainer searchParams={searchParams} />
         <Announcements />
       </div>
     </div>
   );
-};
+}
 
 export default AdminPage;
